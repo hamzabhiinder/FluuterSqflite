187d0d59be2a2a1b312a54d4dc001c11a4b79df0
hamzabhiinder <106860134+hamzabhiinder@users.noreply.github.com>
2023-09-25 06:22:07 +0500

    Update finger_print.dart

--- finger_print/finger_print.dart
+++ finger_print/finger_print.dart
@@ -22,3 +22,42 @@ class FingerPrintAuthentication {
     }
   }
 }
+
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton.icon(
+              onPressed: () async {
+                bool auth = await FingerPrintAuthentication.authentication();
+                log("Authntication $auth");
+
+              },
+              icon: Icon(Icons.fingerprint),
+              label: Text("Finger Print"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
